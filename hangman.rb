8c65a7d8d242153832f47510587602171e8421a3
ftmanspile <codfan15@gmail.com>
2018-01-01 14:26:58 -0500

    Fixed Possible Choices

--- hangman.rb
+++ hangman.rb
@@ -13,18 +13,17 @@ end
 $turns = 10
 
 $letters_guessed = []
-
+$possible_guesses = ('a'..'z').to_a
 def user_input()
-	possible_guesses = ('a'..'z').to_a
 	puts "Your word contains #{$letter_count}"
 	puts "You have #{$turns} turns left."
 	$turns = $turns - 1
-	print possible_guesses
+	print $possible_guesses
 	$letter = gets.chomp();
 	$letters_guessed.push($letter)
 	puts $letters_guessed
-	possible_guesses = possible_guesses - letters_guessed
-	
+	$possible_guesses = $possible_guesses - $letters_guessed
+	print $possible_guesses
 	if ($letter.length) > 1
 		puts "Invalid input!"
 		play_game
@@ -33,14 +32,17 @@ end
 
 play_game
 
-if $turns == 0 
-	abort ("")
-elsif $letter == $word
-	print ("Congrats")
-end
+
 while $turns >= 1 
 	user_input
 end
+if $turns == 0
+	print "YOU DIED"
+end
+#elsif $letter == $word
+	#print ("Congrats")
+
+
 
 
 
